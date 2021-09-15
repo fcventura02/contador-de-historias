51a04c922dc61a6f8fdbc397209b31e1e6729db0
fcventura02 <filipecvr4@gmail.com>
2021-09-14 22:43:00 -0300

    created script pre history

---
+++ scripts/prehistorias.js
@@ -0,0 +1,78 @@
+const HeroEquipment = {
+  sword:
+    "espada de são jorge, uma planta mágica que se o usuario dela for o escolhido ela se torna uma espada de combate.",
+  spiritualMagic:
+    "magia espiritual, uma abilidade que consiste em concentrar a magia ao redor do mago para evocar seus feitiços.",
+  cupHolderMagic:
+    "porta copo magico, que serve para aqueles que nao possuem ou possuem poucas abilidades mágicas, este item é capas de controlar os 4 elementos próximos de quem a controla.",
+  mythicalSphere:
+    "esfera mítica, um item ultra raro onde so aqueles escolhidos sao capazes de controlar-lá, possuindo enorme manipulação mágica e de enorme poder.",
+};
+const HeroGenre = {
+  dragon:
+    "este herói é da família Dragonia, uma espécie famosa por serem grandes manipuladores do fogo e de grande resistência",
+  human:
+    "este herói é da família Imanity, uma espécie sem qualquer tipo de afinidade com magia, mas dotada de bastante inteligência",
+  dryad:
+    "este herói é da família dos Elfos, uma espécie que possui maior afinidade com magia, sendo a única espécie capaz de utilizar mais de uma magia ao mesmo tempo",
+  goblin:
+    "este herói é da família dos goblins, uma espécie da floresta com pouca afinidade com magia e possuem uma boa resistência",
+};
+const Incident = {
+  alienInvasion:
+    "Uma invasão está ocorrendo, e não é uma invasão qualquer. São Aliens que vieram dominar todo este lugar.",
+  chemicalWarfare:
+    "Após uma enorme tentativa para evitar uma guerra, o maior inimigo de todos e o mais maligno ser, iniciou a guerra ativando suas enormes bombas jogando-as em seus inimigos e contraventores.",
+  endOfTimes:
+    "Como diziam as profecias, chegou o fim dos tempos, onde a natureza quer expulsar os seres existentes e recomeçar com uma nova vida.",
+  portal:
+    "Após seres realizarem um ritual, eles abriram um portal para o submundo",
+};
+const Location = {
+  darkForest: {
+    name: "na Floresta Sombria",
+    description:
+      "uma floresta amaldicioda onde aqueles que se atreveram adentrar nela nunca mais voltaram.",
+    bg: [
+      "https://cdn.pixabay.com/photo/2018/01/09/20/24/wood-3072434__340.jpg",
+      "https://cdn.pixabay.com/photo/2016/11/12/11/51/animals-1818690__340.jpg",
+      "https://cdn.pixabay.com/photo/2021/08/28/11/13/walking-6580711__340.jpg",
+    ],
+  },
+  galaxyOfDesires: {
+    name: "na Galaxia dos Desejos",
+    description: "um lugar maravilhoso, onde as lendas relatam que ",
+    bg: [
+      "https://cdn.pixabay.com/photo/2011/12/14/12/18/space-11099__340.jpg",
+      "https://cdn.pixabay.com/photo/2017/08/06/12/11/people-2591928__340.jpg",
+      "https://cdn.pixabay.com/photo/2017/03/28/22/55/night-photograph-2183637__340.jpg",
+    ],
+  },
+  mountainOfTheWise: {
+    name: "na Montanha dos sábios",
+    description:
+      "Este lugar onde abita um enorme ser de grande sabedoria do mundo, podendo prever até mesmo seu futuro.",
+    bg: [
+      "https://cdn.pixabay.com/photo/2015/02/18/11/50/mountain-range-640617__340.jpg",
+      "https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832__340.jpg",
+      "https://cdn.pixabay.com/photo/2017/09/08/09/26/volcano-2728184__340.png",
+    ],
+  },
+  bermudaTriangle: {
+    name: "no Triângulo das Bermudas",
+    description:
+      "Um lugar lendario amaldiçoado e temido por muitos marinheiros, dizem que neste lugar existem varios monstros que vivem dentro do mar a espreita daqueles que se atreverem à adentar nessas águas.",
+    bg: [
+      "https://cdn.pixabay.com/photo/2014/11/19/01/01/churning-536850__340.jpg",
+      "https://cdn.pixabay.com/photo/2018/03/23/19/34/storm-3254676__340.jpg",
+      "https://cdn.pixabay.com/photo/2016/08/17/01/39/mystery-1599527__480.jpg",
+    ],
+  },
+};
+const Tale = {
+  fairies: "é um conto de fadas",
+  dragons: "é um conto da era dos dragões",
+  middleAges: "é um conto da era medieval",
+};
+
+export { HeroEquipment, HeroGenre, Incident, Location, Tale };
